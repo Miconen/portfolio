1017fd483302ba0e4308b51fa67bb59b237d4e57
Mico <micropizza.mc@gmail.com>
2022-08-19 15:51:58 +0300

    Moved from using github api to using localized json data

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
 import Project from "./Project";
+import Loading from "./Loading.tsx";
+import repositories from "./Repositories.json";
 import "./Projects.scss";
 
 interface IGitData {
@@ -11,28 +13,38 @@ const Projects = () => {
   const [githubData, setGithubData] = useState<IGitData[]>([]);
   const [isLoading, setLoading] = useState(true);
 
-  const fetchData = async () => {
-    const response = await fetch(
-      `https://api.github.com/users/${GITHUB_USER}/repos`
-    );
-    let json = await response.json();
-    setGithubData(json);
-    setLoading(false);
-    console.log(json);
-  };
+    
+  // const fetchData = async () => {
+  //   const response = await fetch(
+  //     `https://api.github.com/users/${GITHUB_USER}/repos`
+  //   );
+  //   let json = await response.json();
+  //   setGithubData(json);
+  //   setLoading(false);
+  //   console.log(json);
+  // };
+
+    const fetchData = () => {
+        let response = [];
+        Object.keys(repositories).map((key) => {
+            response.push(repositories[key]); 
+        });
+        setGithubData(response);
+        setLoading(false);
+    }
 
   useEffect(() => {
     fetchData();
   }, []);
 
-  if (isLoading) return <p>Loading...</p>;
+  if (isLoading) return <Loading />;;
 
   return (
-    <ul className="projects">
-      {githubData.map((repo) => (
-        <Project key={repo.name} repo={repo} />
-      ))}
-    </ul>
+        <ul className="projects">
+          {githubData.map((repo) => (
+            <Project key={repo.name} repo={repo} />
+          ))}
+        </ul>
   );
 };
 
