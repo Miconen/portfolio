3930996a1a849f602646c16c2c05320f58c070d1
Rintala Mico <s2200708@BC-C02J649XQ6LR-MacBook-Air.local>
2022-09-06 13:48:47 +0300

    Updated astro.config for github pages deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [react()],
-	base: '.',
-	output: 'static',
+    integrations: [react()],
+    site: "https://miconen.github.io",
+    base: "/portfolio",
+    output: "static",
 });
